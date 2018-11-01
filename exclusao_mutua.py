569c9577310b05335417574f5178e933aa20d1b2
Michael Santos <michael-santos@outlook.com.br>
2018-11-01 03:10:40 -0300

    Adicionadas exibições de filas dos recusos em uso e solicitados

--- exclusao_mutua.py
+++ exclusao_mutua.py
@@ -9,14 +9,48 @@ NUMPROCESS = 3
 
 
 #############################################################################
+# Imprimir resultados
+#############################################################################
+
+# Imprime a fila de resursos em uso
+def imprimirRecursosEmUso(listaRecursosEmUso):
+	print("#################################################################")
+	
+	if not listaRecursosEmUso:
+		print("# Não há recursos sendo utilizados")
+	
+	for registro in listaRecursosEmUso:
+		print("# Recurso: {} | Tempo: {}".format(registro["nomeRecurso"], registro["tempo"]))
+	
+	print("#################################################################")
+
+
+# Imprime a fila de resursos solicitados
+def imprimirRecursosSolicitados(listaRecursosSolicitados):
+	print("#################################################################")
+	
+	if not listaRecursosSolicitados:
+		print("# Não há recursos solicitados")
+	
+	for registro in listaRecursosSolicitados:
+		print("# Recurso: {} | Marca de Tempo: {} | Tempo: {}".format(registro["nomeRecurso"], 
+			registro["marcaTempo"], registro["tempo"]))
+	
+	print("#################################################################")
+
+#############################################################################
 # Consumo de recursos
 #############################################################################
 
 # Consome os recursos em uso (a cada 5 segundos decrementa o contador de tempo do recurso)
-def consumir(idPross, portaPross, clockInicial, listaRecursosEmUso, listaRecursosSoliciatados, listaRecursosACK):
+def consumir(idPross, portaPross, clockInicial, listaRecursosEmUso, listaRecursosSolicitados, listaRecursosACK, listaProcessosProximos):
 	
 	while(True):
-		time.sleep(5)
+		print("\nRecursos em uso")
+		imprimirRecursosEmUso(listaRecursosEmUso)
+		print("\nRecursos solicitados")
+		imprimirRecursosSolicitados(listaRecursosSolicitados)
+		time.sleep(10)
 		for i in range(len(listaRecursosACK)):
 			listaRecursosACK[i]["tempo"] = listaRecursosACK[i]["tempo"] - 1
 			if listaRecursosACK[i]["tempo"] == 0:
@@ -38,7 +72,7 @@ def updateClock(mensagem, clockInicial):
 
 
 # Identificar o que a mensagem fará
-def gerenciarRecurso(mensagemJson, clockInicial, listaRecursosEmUso, listaRecursosSoliciatados, listaRecursosACK, portaPross):
+def gerenciarRecurso(mensagemJson, clockInicial, listaRecursosEmUso, listaRecursosSolicitados, listaRecursosACK, listaProcessosProximos, portaPross):
 	mensagem = json.loads(mensagemJson.decode('utf-8'))
 	print(mensagem)
 
@@ -55,14 +89,14 @@ def gerenciarRecurso(mensagemJson, clockInicial, listaRecursosEmUso, listaRecurs
 #############################################################################
 
 # Recebe mensagens via socket UDP
-def receiver(idPross, portaPross, clockInicial, listaRecursosEmUso, listaRecursosSoliciatados, listaRecursosACK):
+def receiver(idPross, portaPross, clockInicial, listaRecursosEmUso, listaRecursosSolicitados, listaRecursosACK, listaProcessosProximos):
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	server_address = ('', PORTAS_PROCESSOS[portaPross])
 	sock.bind(server_address)
 
 	while(True):
 		data, address = sock.recvfrom(4096)
-		gerenciarRecurso(data, clockInicial, listaRecursosEmUso, listaRecursosEmUso, listaRecursosSoliciatados, portaPross)
+		gerenciarRecurso(data, clockInicial, listaRecursosEmUso, listaRecursosEmUso, listaRecursosSolicitados, listaProcessosProximos, portaPross)
 
 # Envia mensagens em Unicast
 def sender(mensagem, portaPross):
@@ -87,8 +121,9 @@ def enviarBroadcast(mensagem, portaPross):
 
 # Inicio programa
 listaRecursosEmUso=[]
-listaRecursosSoliciatados = []
+listaRecursosSolicitados = []
 listaRecursosACK = []
+listaProcessosProximos = []
 
 portaPross = int(input("Digite a porta do processo (0-2): "))
 idPross = int(input("Digite o id do processo: "))
@@ -96,11 +131,11 @@ clockInicial = input("Digite o clock inicial: ")
 clockInicial = [int(clockInicial)]
 
 # Cria thead responśvel por receber as mensagens
-t1 = threading.Thread(target=receiver, args=(idPross, portaPross, clockInicial, listaRecursosEmUso, listaRecursosSoliciatados, listaRecursosACK))
+t1 = threading.Thread(target=receiver, args=(idPross, portaPross, clockInicial, listaRecursosEmUso, listaRecursosSolicitados, listaRecursosACK, listaProcessosProximos))
 t1.start()
 
 # Cria thread que é responsável por consumir os recursos
-t2 = threading.Thread(target=consumir, args=(idPross, portaPross, clockInicial, listaRecursosEmUso, listaRecursosSoliciatados, listaRecursosACK))
+t2 = threading.Thread(target=consumir, args=(idPross, portaPross, clockInicial, listaRecursosEmUso, listaRecursosSolicitados, listaRecursosACK, listaProcessosProximos))
 t2.start()
 
 # Thread principal é responsável por solicitar recursos aos outros processos
@@ -118,15 +153,15 @@ while(True):
 		if listaRecursosEmUso[i]["nomeRecurso"] == recurso:
 			podeSocilitar = False
 
-	for i in range(len(listaRecursosSoliciatados)):
-		if listaRecursosSoliciatados[i]["nomeRecurso"] == recurso:
+	for i in range(len(listaRecursosSolicitados)):
+		if listaRecursosSolicitados[i]["nomeRecurso"] == recurso:
 			podeSocilitar = False
 	
 	if podeSocilitar == False:
 		print("Recurso já está em uso ou já foi solicitado!")
 	else:
-		listaRecursosSoliciatados.append( {"nomeRecurso": recurso, "marcaTempo": str(clockInicial[0]+1).zfill(3) + str(idPross).zfill(3), "tempo": tempo} )
-		listaRecursosSoliciatados.sort(key=lambda x:x["marcaTempo"])
+		listaRecursosSolicitados.append( {"nomeRecurso": recurso, "marcaTempo": str(clockInicial[0]+1).zfill(3) + str(idPross).zfill(3), "tempo": tempo} )
+		listaRecursosSolicitados.sort(key=lambda x:x["marcaTempo"])
 		mensagemJson = {"tipoMensagem": "requisicao" , "marcaTempo": str(clockInicial[0]+1).zfill(3) + str(idPross).zfill(3), "recurso": recurso, "tempo": tempo}
 		mensagem = json.dumps(mensagemJson)
 		enviarBroadcast(mensagem, portaPross)
